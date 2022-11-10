c417ee98f52bbb3d00f9e38be12f96b988101b8c
Brannon <bbreau6>
2022-11-10 09:48:34 -0500

    First Commit

---
+++ F7CCKLBIEEOC3Z9.py
@@ -0,0 +1,91 @@
+#!/usr/bin/python
+import MFRC522
+import signal
+import ConfigParser
+import os
+
+# Read config file
+Config = ConfigParser.ConfigParser()
+Config.read("/home/pi/MFRC522-python/rfidconfig.txt")
+
+# From https://wiki.python.org/moin/ConfigParserExamples
+def ConfigSectionMap(section):
+  dict1 = {}
+  options = Config.options(section)
+  for option in options:
+	try: dict1[option] = Config.get(section, option)
+	if dict1[option] == -1:
+               	DebugPrint("skip: %s" % option)
+	except:
+               print("exception on %s!" % option)
+               dict1[option] = None
+  return dict1
+
+# Send action to mplayer
+def TagToMplayer (strTag):
+  try:
+	# Read tag attributes from config 
+	strActionType = ConfigSectionMap(strTag)['actiontype']
+	strFileUrlFunction = ConfigSectionMap(strTag)['fileurlfunction']
+	strDescription = ConfigSectionMap(strTag)['description']
+
+  	if  strActionType == "URL" or strActionType == "File":
+	  strAction = "loadfile " + strFileUrlFunction
+	  # Show what will be played
+	  print ("Audio " + strDescription + " from " + strFileUrlFunction)
+
+	elif strActionType == "Playlist":
+	  strAction = "loadlist " + strFileUrlFunction
+	  # Show what will be played
+	  print ("Audio " + strDescription + " from " + strFileUrlFunction)
+
+	elif strActionType == "Function":
+	  strAction = strFileUrlFunction
+ 	  # Show what action will be performed
+	  print ("Function " + strFileUrlFunction)
+
+	elif strActionType == "OS":
+	  strAction = ""
+	  # Execute on OS, be careful, running as ROOT
+	  os.system (strFileUrlFunction)
+
+	else:
+	  strAction = ""
+
+	# Write to cfile
+	with open(cfile, "w") as myfile:
+	  myfile.write(strAction+"\n")
+ 
+  except:
+	pass
+
+continue_reading = True
+MIFAREReader = MFRC522.MFRC522()
+
+# Control file used by mplayer
+cfile = "/tmp/mplayer-control"
+
+def end_read(signal, frame):
+  global continue_reading
+  continue_reading = False
+  print "Ctrl+C captured, ending read."
+  MIFAREReader.GPIO_CLEEN()
+
+signal.signal(signal.SIGINT, end_read)
+
+# Never stop reading
+while continue_reading:
+  (status,TagType) = MIFAREReader.MFRC522_Request(MIFAREReader.PICC_REQIDL)
+  if status == MIFAREReader.MI_OK:
+    print "Card detected"
+  (status,backData) = MIFAREReader.MFRC522_Anticoll()
+  if status == MIFAREReader.MI_OK:
+    strbackData = str(backData[0])+","+str(backData[1])+","+str(backData[2])+","+str(backData[3])+","+str(backData[4])
+
+    print "Card read UID: " + strbackData
+
+    # Do something (based on card and config file)
+    TagToMplayer(strbackData)
+
+
+
---
+++ Root/Playlists/Example.pls
@@ -0,0 +1,31 @@
+[playlist]
+
+File1=/media/exfat/Music/R/Red Hot Chilli Peppers/Greatest Hits [Warner Bros]/02 - Give It Away.mp3
+
+Title1=Give It Away - Red Hot Chili Peppers
+
+Length1=284
+
+File2=/media/exfat/Music/V/Various Artists/RENT/Disc 1/1-14 - Over the Moon.mp3
+
+Title2=Over the Moon - Various Artists
+
+Length2=383
+
+File3=/media/exfat/Music/S/Slayer/Hell Awaits/06 - Crypts of Eternity.mp3
+
+Title3=Crypts of Eternity - Slayer
+
+Length3=400
+
+File4=/media/exfat/Music/S/Styx/Cyclorama/07 - Styx - Yes I Can.mp3
+
+Title4=Yes I Can - Styx
+
+Length4=231
+
+File5=/media/exfat/Music/G/Greenday/Warning/12 Macy's Day Parade.mp3
+
+Title5=Macy's Day Parade - Green Day
+
+Length5=214
